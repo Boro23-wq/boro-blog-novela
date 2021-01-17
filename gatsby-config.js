ef2d51e7f385bc2887427329ff547ca6ce0e5fd3
Boro23 <404slimboysavage@gmail.com>
2021-01-17 14:03:54 +0530

    added-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Blog | Boro`,
+    image: `https://images.unsplash.com/photo-1519337265831-281ec6cc8514?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80`,
     name: `Boro`,
     siteUrl: `https://boro-blog.netlify.app`,
     description: `Technical blog on Full-stack technologies, development, and best practices in general.`,
